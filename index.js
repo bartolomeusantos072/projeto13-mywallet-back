3b23f9772aeafd0bb8fe6c3f220b8e24cfc24621
Uenderss <meajudabarto@gmail.com>
2022-06-29 01:59:27 -0300

    conectar ao banco mywalet

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-import express from "express";
-import {mongoClient} from "mongodb";
-import bcrypt from 'bcrypt';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from 'express';
+import { MongoClient } from "mongodb";
+import chalk from 'chalk';
+// import bcrypt from "bcrypt";
+import cors from "cors";
+import dotenv from "dotenv";
 dotenv.config();
 
-const mongoClient = new mongoClient(process.env.MONGO_URI);
+const app=express();
+app.use(cors());
+app.use(express.json());
+
+const port =process.env.PORT ||5000;
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
-mongoClient.connect();
+const promise = mongoClient.connect();
+promise.then(() => {
+    db = mongoClient.db(process.env.DATABASE);
+  }).catch((e) => {
+    console.log(chalk.red.bold("Falha ao conectar no banco"));
+  });
+
+app.listen(port, ()=>{
+    console.log(chalk.green.bold("Servidor Conectado na porta ",port));
+})
