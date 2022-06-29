01185b3cf5ffa8686a321d1b6fa2bc908a02c379
Uenderss <meajudabarto@gmail.com>
2022-06-29 04:13:05 -0300

    correcao do sign-in sign-up e meus-dados

--- index.js
+++ index.js
@@ -25,61 +25,72 @@ promise
     console.log(chalk.red.bold("Falha ao conectar no banco"));
   });
 
+app.post("/sign-up", async (req, res) => {
+    const user = req.body;
+    const { name, email, password, password_confirmation }=user;
+  
+    const passwordHash = bcrypt.hashSync(user.password, 10);
+    
+    const userSchema = Joi.object({
+      name: Joi.string().alphanum().min(3).max(30).required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(3).max(15).required(),
+      password_confirmation: Joi.any()
+        .valid(Joi.ref("password"))
+        .required()
+    });
+    
+    const { error } = userSchema.validate(user);
+    if(error){
+      return res.status(422).send(error.details.map(detail => detail.message));
+    }
+    try {
+      await db.collection("users").insertOne({ ...user, password: passwordHash });
+      res.sendStatus(201);
+    } catch {
+      res.status(401);
+    }
+  });
+
 app.post("/sign-in", async (req, res) => {
   const { email, password } = req.body;
 
-  const emailSchema = Joi.object({
+
+  const login={email,password};
+  
+  const signinSchema = Joi.object({
     email: Joi.string().email().required(),
+    password: Joi.string().min(3).max(15).required()
   });
-  const { erro } = emailSchema.validate(email);
-  if (erro) {
-    return res.sendStatus(422);
+  
+  const { error } = signinSchema.validate(login);
+  if (error) {
+    return res.status(422).send(error.details.map(detail => detail.message));
   }
+
   try {
-    const user = await db.collection("user").findOne({ email });
+    const user = await db.collection('users').findOne({ email });
+    console.log(user);
     if (user && bcrypt.compareSync(password, user.password)) {
+      
       const token = uuid();
-      await db.collection("sessions").insertOne({ token, userId: user_id });
+  
+      await db.collection('sessions').insertOne({ token, userId: user._id });
+  
       res.send(token);
     } else {
       res.sendStatus(401);
     }
   } catch {
-    res.status(401).send("Erro no Try Cat do sing-in");
-    // res.sendStatus(401)
+    res.sendStatus(401)
   }
 });
 
-app.post("/sign-up", async (req, res) => {
-  const {user} = req.body;
-  const { name, email, password }=user;
 
-  const passwordHash = bcrypt.hashSync(user.password, 10);
-  
-  const userSchema = Joi.object({
-    name: Joi.string().alphanum().min(3).max(30).required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().min(3).max(15).required(),
-    password_confirmation: Joi.any()
-      .valid(Joi.ref("password"))
-      .required()
-      .options({ language: { any: { allowOnly: "must match password" } } })
-  });
-  
-  const { error } = userSchema.validate(user);
-  if(error){
-    return res.status(422).send(error.details.map(detail => detail.message));
-  }
-  try {
-    await db.collection("users").insertOne({ ...user, password: passwordHash });
-    res.sendStatus(201);
-  } catch {
-    res.status(401);
-  }
-});
 
 app.get("/meus-dados",async (req,res)=>{
   const {authorization}=req.headers;
+  console.log(authorization);
   const token=authorization?.replace('Bearer ','');
 
   if(!token){
